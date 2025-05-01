f65efcd6cb87d709c52c38fd230295ee6eaab7ac
YuriLopess <costalopesyuri@gmail.com>
2025-05-01 16:05:33 -0300

    feat: add service package and paymentService class

---
+++ src/main/java/io/github/yurilopess/paymentMicroservices/service/PaymentService.java
@@ -0,0 +1,47 @@
+package io.github.yurilopess.paymentMicroservices.service;
+
+import io.github.yurilopess.paymentMicroservices.dto.PaymentDto;
+import io.github.yurilopess.paymentMicroservices.model.Payment;
+import io.github.yurilopess.paymentMicroservices.repository.PaymentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PaymentDto> getAll(Pageable pageable) {
+        return repository.findAll(pageable)
+                .map(p -> modelMapper.map(p, PaymentDto.class));
+    }
+
+    public PaymentDto getById(UUID id) {
+        Payment payment = repository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return modelMapper.map(payment, PaymentDto.class);
+    }
+
+    public PaymentDto putPayment(UUID id, PaymentDto dto) {
+        Payment payment = modelMapper.map(dto, Payment.class);
+        payment.setId(id);
+        payment = repository.save(payment);
+
+        return modelMapper.map(payment, PaymentDto.class);
+    }
+
+    public void deletePayment(UUID id) {
+        repository.deleteById(id);
+    }
+}
